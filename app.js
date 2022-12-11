0968fb68e154e075e09ca4d74a9525feefe57bad
Ayan Bera <66629653+ayanbera25@users.noreply.github.com>
2022-12-12 01:54:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 
-var qrcode=document.getElementById('qrcode');
-var user_input=document.getElementById('name');
-var gen_btn=document.getElementById('form-btn');
-var error=document.getElementById('error');
-var scantext=document.getElementById('scan-text');
-var download_qr = document.getElementById('download-qr');
-var download_btn = document.getElementById('download-btn');
+const qrcode=document.getElementById('qrcode');
+const user_input=document.getElementById('name');
+const gen_btn=document.getElementById('form-btn');
+const error=document.getElementById('error');
+const scantext=document.getElementById('scan-text');
+
+const resetbtn=document.getElementById('reset-btn');
+
 
 qrcode.style.display="none";
 error.textContent="";
+// generate button functionality
 gen_btn.addEventListener("click",()=>{
     if(user_input.value==""){
-        error.textContent="Please enter a text or url";
+        error.textContent="Oops! Please enter a text or url";
     }
     else{
         error.textContent="";
@@ -19,11 +21,13 @@ gen_btn.addEventListener("click",()=>{
         url=`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${user_input.value}`;
         qrcode.src=url;
         scantext.innerHTML="scan this qr code."
-        download_btn.style.display="block";
-        download_btn.addEventListener("click",()=>{
-            download_qr.href=url+".jpeg";
-            download_qr.download=user_input.value +".jpeg";
-        })
+        
+        // resetbutton functuionality
+        resetbtn.addEventListener("click",()=>{
+            qrcode.src="";
+            qrcode.style.display="none";
+            scantext.innerHTML="";
+        });
     } 
     
 })
